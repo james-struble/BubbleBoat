d1a022341e7c30cc6f2941938f1cde91603e0f28
james-struble <jas2hf@umsystem.edu>
2025-01-26 01:25:19 -0600

    Game Over UI Added
    Retry button reloads GameScene

---
+++ Assets/GameOverUI.cs
@@ -0,0 +1,59 @@
+using System;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameOverUI : MonoBehaviour
+{
+    [SerializeField] GameManager gameManager; // Reference to GameManger script for event listening, state checking, and function calling
+    [SerializeField] private TextMeshProUGUI gameOverText;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    //[SerializeField] private TextMeshProUGUI hiScoreText;
+    [SerializeField] private Button retryButton;
+
+    void Awake()
+    {
+        // TextMeshPro method of tracking if button has been pressed
+        retryButton.onClick.AddListener(ResetGame); // Calls ResetGame function if button pressed
+    }
+
+    void Start()
+    {
+        Hide(); // Hide all Game Over UI elements
+        gameManager.OnStateChanged += GameManager_OnStateChanged; // Set up event listener for OnStateChanged, will call GameManager_OnStateChanged function when event fires
+    }
+
+    private void GameManager_OnStateChanged(object sender, EventArgs e)
+    {
+        if (gameManager.IsGameOver()) // Check if game has ended
+        {
+            Show(); // Show all Game Over UI elements
+            scoreText.text = gameManager.GetScore().ToString(); // Change scoreText to score of current game
+            //hiScoreText.text = gameManager.GetHighScore().ToString(); // Change hiScoreText to Highscore saved in PlayerPrefs
+        }
+
+        // if (gameManager.IsGameWon())
+        // {  
+        //     Show(); // Show all Game Over UI elements
+        //     gameOverText.text = "YOU WIN";
+        //     scoreText.text = gameManager.GetScore().ToString(); // Change scoreText to score of current game
+        //     hiScoreText.text = gameManager.GetHighScore().ToString(); // Change hiScoreText to Highscore saved in PlayerPrefs
+        // }
+    }
+    
+    private void Show() 
+    {
+        gameObject.SetActive(true); // Show Game Over UI Elements
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false); // Hide Game Over UI Elements
+    }
+
+    private void ResetGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);  // Reset game scene
+    }
+}
